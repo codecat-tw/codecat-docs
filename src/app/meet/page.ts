f5cdbbe4980d9f0695da8db4745ce04ca3c3836c
程式貓 <codecat.tw@gmail.com>
2025-10-04 10:56:58 +0800

    Update page.ts

--- src/app/meet/page.ts
+++ src/app/meet/page.ts
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function RedirectPage() {
-  redirect("https://meet.google.com/");
+  redirect("https://meet.google.com/tjo-cyeg-nmb");
 }
